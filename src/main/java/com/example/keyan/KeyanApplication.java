e1265eae06c4e80b7b9290d7fb244f2ebdf506f3
palpiate-wx <2281342596@qq.com>
2022-07-24 18:57:48 +0800

    初始化工程

---
+++ src/main/java/com/example/keyan/KeyanApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyanApplication.class, args);
+	}
+
+}
